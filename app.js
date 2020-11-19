899b31326efe1783e71716f6c7d2d7b64f89a0d9
unknown <peters0470@gmail.com>
2020-11-18 21:50:06 -0600

    added generate page function to new file and cleaned up main file

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));   
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
-printProfileData(profileDataArgs);
